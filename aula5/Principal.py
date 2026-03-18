b063ae2733564ef3a6826bc51245e69ed53cdafb
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-03-18 10:35:30 -0300

    Update Principal.py

--- aula5/Principal.py
+++ aula5/Principal.py
@@ -1,22 +1,53 @@
 from clima import Clima
+
 lista = []
+nome_base = "base.csv"
 
-linha = " 2020, Janeiro, Quente, muita"
-dadosLinha = linha.split(",")
+try:
+    #abrindo o arquivo o modo leitura
+    leitor = open(nome_base, "r", encoding = "utf-8")
 
-obj_clima = Clima(dadosLinha[0], dadosLinha[1], dadosLinha[2], dadosLinha[3])
+    for linha in leitor:
+        dados_linha = linha.split(",")
+        obj_clima = Clima(dados_linha[0], dados_linha[1], dados_linha[2], dados_linha[3])
 
-if obj_clima not in lista:
-    lista.append(obj_clima)
+        if obj_clima not in lista:
+            lista.append(obj_clima)
 
-#segunda linha
-linha = " 2020, Janeiro, Frio, pouca"
-dadosLinha  = linha.split(",")
 
-obj_clima = Clima(dadosLinha[0], dadosLinha[1], dadosLinha[2], dadosLinha[3])
+    for item in lista:
+        print(item)    
 
-if obj_clima not in lista:
-    lista.append(obj_clima)
 
-for c in lista:
-    print(c)
+    #fechando o arquivo
+    leitor.close()
+
+except Exception as e:
+    print("Ocorreu algum erro", e)
+
+
+
+
+
+# from clima import Clima
+# lista = []
+
+# linha = " 2020, Janeiro, Quente, muita"
+# dadosLinha = linha.split(",")
+
+# obj_clima = Clima(dadosLinha[0], dadosLinha[1], dadosLinha[2], dadosLinha[3])
+
+# if obj_clima not in lista:
+#     lista.append(obj_clima)
+
+# #segunda linha
+# linha = " 2020, Janeiro, Frio, pouca"
+# dadosLinha  = linha.split(",")
+
+# obj_clima = Clima(dadosLinha[0], dadosLinha[1], dadosLinha[2], dadosLinha[3])
+
+# if obj_clima not in lista:
+#     lista.append(obj_clima)
+
+# for c in lista:
+#     print(c)
