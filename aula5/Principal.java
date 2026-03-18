ccc113fc8660733ffc2ddbbd7ea46e8376ce633c
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-03-18 10:47:44 -0300

    Create Principal.java

---
+++ aula5/Principal.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+        ArrayList<Clima> lista = new ArrayList<>();
+        
+        //simulando que estamos lendo linha a linha do arquivo
+        //primeira linha
+        String linha = " 2020, Janeiro, Quente, muita";
+        String dadosLinha[] = linha.split(",");
+        Clima objClima = new Clima(dadosLinha[0], dadosLinha[1], dadosLinha[2], dadosLinha[3]);
+
+        if (!lista.contains(objClima)) {
+            lista.add(objClima);
+        }
+
+        //segunda linha
+        linha = " 2020, Janeiro, Frio, pouca";
+        dadosLinha  = linha.split(",");
+        objClima = new Clima(dadosLinha[0], dadosLinha[1], dadosLinha[2], dadosLinha[3]);
+        
+        if (!lista.contains(objClima)) {
+            lista.add(objClima);
+        }
+
+        for (Clima c : lista) {
+            System.out.println(c);
+        }
+    }
+}
