8c368fffc89c340a339b63f2cfdf995dea8c6867
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-03-25 09:53:03 -0300

    Create Processo.java

---
+++ aula6/Processo.java
@@ -0,0 +1,30 @@
+public class Processo {
+    public int id;
+    public String descricao;
+
+    public Processo(int id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    //Identificar a chave primaria
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Processo other = (Processo) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    //definir um objeto
+    @Override
+    public String toString() {
+        return "Processo [id=" + id + ", descricao=" + descricao + "]";
+    }
+}
