a4e726b6f560baa7e726e421a6701fdf2beb3c01
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-04-01 07:33:38 -0300

    Add Clima class with attributes and methods

--- Atividade 1/Clima.java
+++ Atividade 1/Clima.java
@@ -1,1 +1,28 @@
+public class Clima {
+	  String ano;
+    String mes;
+    String temperatura;
+    String precipitacao;
 
+    public Clima(String ano, String mes, String temperatura, String precipitacao) {
+        this.ano = ano;
+        this.mes = mes;
+        this.temperatura = temperatura;
+        this.precipitacao = precipitacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Clima [ano = " + this.ano + ", mes = " + this.mes + ", temperatura = " + this.temperatura + ", precipitacao = " + this.precipitacao+ "]";         
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; 
+        if (obj == null || !(obj instanceof Clima)) return false; 
+
+        Clima c = (Clima) obj;
+
+        return this.ano.equals(c.ano) && this.mes.equals(c.mes); 
+    }
+}
