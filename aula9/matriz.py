f9ef2bf99e5dc7497b966524e830d94303f0a11f
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-04-15 09:33:36 -0300

    Add Matriz class with matrix display and conversion methods

---
+++ aula9/matriz.py
@@ -0,0 +1,26 @@
+from dado import Dado
+
+class Matriz:
+
+    #public static void inicializarMatriz(int matriz[][], int qtLinhas, int qtdColunas):
+    @staticmethod
+    def exibir_matriz(matriz, qtd_linhas, qtd_colunas):
+        for i in range(qtd_linhas):
+            for j in range(qtd_colunas):
+                print(f"{matriz[i][j]} + \t", end="")
+            
+            print()
+
+    @staticmethod
+    def converter_matriz(matriz, qtd_linhas, qtd_colunas, lista):
+        for i in range(qtd_linhas):
+            for j in range(qtd_colunas):
+                if matriz[i][j] != 0:
+                    lista.append( Dado(matriz[i][j], i, j))
+
+    @staticmethod
+    def exibir_lista(lista):
+        for item in lista:
+            print(item)
+
+        print(f"Total de elementos: {len(lista)}")
