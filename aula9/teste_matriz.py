2537a3406bbf110b7d1732a358b4e3b920cca33a
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-04-15 09:34:04 -0300

    Add teste_matriz.py for matrix operations

---
+++ aula9/teste_matriz.py
@@ -0,0 +1,17 @@
+from matriz import Matriz
+
+diemnsao = 10
+matriz  = [[0 for _ in range(dimensao)] for _ in range(dimensao)]
+
+#gerando uma matriz especial
+matriz[2][4] = 1
+matriz[4][6] = 1
+matriz[7][1] = 1
+matriz[8][3] = 1
+
+Matriz.exibir_matriz(matriz, dimensao, dimensao)
+
+lista_especial = []
+
+Matriz.converter_matriz(matriz, dimensao, dimensao, lista_especial)
+Matriz.exibir_lista(lista_especial)
