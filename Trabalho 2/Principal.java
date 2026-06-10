2fcc67eaec7d870775208ee3700e1fa5fa42bf8e
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-06-10 09:21:28 -0300

    Update file path for mapa.csv

--- Trabalho 2/Principal.java
+++ Trabalho 2/Principal.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Principal {
     public static void main(String[] args) {
         ArrayList<String> listaVertices = new ArrayList<>();
-        String arquivoMapa = "./mapa.csv";
+        String arquivoMapa = "mapa.csv";
 
         Grafo.descobrirVertices(arquivoMapa, listaVertices);
         Grafo g = new Grafo(listaVertices);
